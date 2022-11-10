c8f28fad7c4deb8e36ee394a8cbaf1b734ed58d4
Muhammed Ali Kösen <mhalikosen@mhalikosen.com>
2022-11-10 21:00:16 +0300

    Search box created.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Helmet } from 'react-helmet'
+
 const App = () => {
-  return <>Merhasdsdsdsba</>
+  return (
+    <>
+      <Helmet>
+        <body className="min-h-full bg-gradient-to-r from-cyan-500 to-blue-500" />
+      </Helmet>
+      <div className="container mx-auto max-w-2xl">
+        <div className="m-5 p-1 shadow">
+          <input className="bg-transparent focus:border-0 focus:ring-0 w-full h-full" type="text" />
+        </div>
+        <div>sonuçlar</div>
+      </div>
+    </>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import './index.css'
 
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).root.render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
